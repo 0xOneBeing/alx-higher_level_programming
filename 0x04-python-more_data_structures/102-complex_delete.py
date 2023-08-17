8b9557fc281dd2cce7358b5c60edb849caefd243
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-16 22:00:08 -0700

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
-    keys_to_delete = [key for key, val in a_dictionary.items() if val == value]
-
-    for key in keys_to_delete:
-        del a_dictionary[key]
+    if a_dictionary:
+        to_delete = []
+        for k, v in a_dictionary.items():
+            if v == value:
+                to_delete.append(k)
+        for k in to_delete:
+            del a_dictionary[k]
+    return a_dictionary
