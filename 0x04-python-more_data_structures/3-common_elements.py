b99e78c9d5cbe728f4581005f6895f7a9953aa15
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-16 21:45:49 -0700

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    common_elements_set = set()
-    
-    for element in set_1:
-        if element in set_2:
-            common_elements_set.add(element)
-    
-    return common_elements_set
+    if not set_1 or not set_2:
+        return set()
+    return set_1 & set_2
