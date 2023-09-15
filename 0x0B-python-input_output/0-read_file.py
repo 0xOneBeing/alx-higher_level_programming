2ee1b8f8a00f7b196219ee0b227b7b10af68d455
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-09-14 21:41:12 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-"Function reads a text file (UTF-8)"
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            for line in file:
-                print(line, end='')
-    except FileNotFoundError:
-        print(f"The file '{filename}' was not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
