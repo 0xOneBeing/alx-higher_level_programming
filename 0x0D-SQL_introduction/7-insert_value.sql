acb3c72d57cb5d461dd79eb19746c14920cf81a5
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-10-21 03:55:05 -0700

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -5,5 +5,4 @@
 -- name = Best School
 -- The database name will be passed as an argument of the mysql command
 
-USE `hbtn_0c_0`;
 INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
