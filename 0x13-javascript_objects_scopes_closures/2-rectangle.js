78599bc621ceeafccda70b7eaf410bd0c7553b23
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-11-14 08:07:05 -0800

    2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -5,7 +5,7 @@ class Rectangle {
     this.width = w;
     this.height = h;
 
-    if ((w <= 0) || (h <= 0)) {
+    if ((w <= 0) || (h <= 0) || !Number.isInteger(w) || !Number.isInteger(h)) {
       return {};
     }
   }
