26c1869172789e72e884f218b3a1be1b29e8f46d
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2024-01-06 07:33:41 -0800

    100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+require('eeq').get(process.argv[2] + id, function (error, res, body) {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
+      if (error) {
+        console.log(error);
+      }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
+    });
+  }
+});
